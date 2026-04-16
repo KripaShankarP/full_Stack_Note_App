46d69d9ed47227f1ab88cc3014f4d7e0df04279f
Kripa Shankar Pandey <kripashankerpandey1503@gmail.com>
2026-04-16 20:45:24 +0530

    backend changes updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+const mongo_Connection = "mongodb+srv://kripashankerpandey1503_db_user:Pandey1234@cluster0.ona8dug.mongodb.net/?appName=Cluster0"
 
 const connectDb = async()=>{
   try {
-    await mongoose.connect("mongodb://localhost:27017/notesApp")
+    await mongoose.connect(mongo_Connection)
     console.log("Databae Connected Successfully");
     
   } catch (error) {
